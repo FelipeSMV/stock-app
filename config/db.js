85466cbcc834ec306e864256c3854887469e4de4
Felipe <felipe.sanmartin.valenzuela@ciisa.cl>
2025-07-09 00:26:54 -0400

    Prueba3

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
+const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: '127.0.0.1',    // 👈 Forzar IPv4
-  user: 'stock_user',
-  password: 'tu_contraseña_segura',
+  user: 'stock_user',      // 👈 Usuario creado en MariaDB
+  password: 'tu_contraseña_segura', // 👈 Contraseña del usuario
   database: 'stock_app',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+module.exports = pool.promise();
